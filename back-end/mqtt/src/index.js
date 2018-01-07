5997179503815d82cb79f5d488df750c5114f38f
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2018-01-07 00:28:25 -0200

    [Back-end] Recebimento das Estatísticas do ESP;

--- back-end/mqtt/src/index.js
+++ back-end/mqtt/src/index.js
@@ -2,7 +2,8 @@ import mqtt from 'mqtt'
 import knex from 'knex'
 
 const client = mqtt.connect(`mqtt://192.168.0.20`)
-const mqttTopic = 'douglaszuqueto/casa_01/cozinha/temperatura/sensor_01'
+const mqttSensorTopic = 'douglaszuqueto/casa_01/cozinha/temperatura/sensor_01'
+const mqttStatisticsTopic = 'douglaszuqueto/casa_01/cozinha/temperatura/sensor_01/statistics'
 
 const db = knex({
   client: 'mysql',
@@ -17,18 +18,23 @@ const db = knex({
 
 client.on('connect', () => {
   console.log(`Connection successfully to 192.168.0.20`)
-  client.subscribe(mqttTopic)
+  client.subscribe(mqttSensorTopic)
+  client.subscribe(mqttStatisticsTopic)
 })
 
 client.on('message', (topic, message) => {
-  if (mqttTopic !== topic) {
-    return
-  }
-  const temperature = message.toString()
+  if (mqttSensorTopic === topic) {
+    const temperature = message.toString()
 
-  create('Sensor 01', temperature)
-  create('Sensor 02', parseFloat(temperature) + 1)
-  create('Sensor 03', parseFloat(temperature) + 2)
+    create('Sensor 01', temperature)
+    create('Sensor 02', parseFloat(temperature) + 1)
+    create('Sensor 03', parseFloat(temperature) + 2)
+  }
+
+  if (mqttStatisticsTopic === topic) {
+    const statistics = JSON.parse(message.toString())
+    console.log(statistics)
+  }
 })
 
 const create = (topic, temperature) => {
