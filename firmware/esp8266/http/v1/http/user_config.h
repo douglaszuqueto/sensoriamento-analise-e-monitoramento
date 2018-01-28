d90b3d517869b0ec218410b5077c2a595f54b5a9
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2018-01-28 17:23:43 -0200

    [Firmware - ESP8266] Mudança do IP para não dar conflito na rede;

--- firmware/esp8266/http/v1/http/user_config.h
+++ firmware/esp8266/http/v1/http/user_config.h
@@ -6,7 +6,7 @@
 #define WIFI_SSID     "YOUR_SSID"
 #define WIFI_PASSWORD "YOUR_PASS"
 
-IPAddress ip(192, 168, 0, 100);
+IPAddress ip(192, 168, 0, 101);
 IPAddress gw(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
@@ -16,6 +16,6 @@ const char* API_URL = "http://192.168.0.10:8081/";
 
 /* SENSOR */
 
-#define DHT_SENSOR
+#define MCP9808_SENSOR
 
 #endif
