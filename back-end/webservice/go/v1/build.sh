8b18fb098330613fd96f3540ddef1679a55293ab
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2018-02-01 21:31:04 -0200

    [Back-end Go] Alteração do script de build e pasta padrão do binário

--- back-end/webservice/go/v1/build.sh
+++ back-end/webservice/go/v1/build.sh
@@ -4,6 +4,8 @@ export GOPATH=$(pwd)
 
 echo -e "Realizando build da API...\n"
 
-go build -o sensoriamento-api
+go get github.com/gorilla/mux
+go get github.com/go-sql-driver/mysql
+go build -o bin/sensoriamento-api
 
 echo -e "\nBuild realizado com sucesso!"
